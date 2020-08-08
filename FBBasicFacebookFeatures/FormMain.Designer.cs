44c1d9f855363c32e4fedf0fd19449c1991b2d28
dekeltssairi <dekeltsairi@gmail.com>
2020-08-08 03:21:23 +0300

    Add project files.

---
+++ FBBasicFacebookFeatures/FormMain.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace BasicFacebookFeatures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picture_smallPictureBox = new System.Windows.Forms.PictureBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.listBoxEvents = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.buttonSetStatus = new System.Windows.Forms.Button();
+            this.listBoxPosts = new System.Windows.Forms.ListBox();
+            this.pictureBoxEvent = new System.Windows.Forms.PictureBox();
+            this.labelEvents = new System.Windows.Forms.LinkLabel();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.f_CheckinsButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBoxPostComments = new System.Windows.Forms.ListBox();
+            this.linkPosts = new System.Windows.Forms.LinkLabel();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.comboBoxActionType = new System.Windows.Forms.ComboBox();
+            this.dataGridViewActions = new System.Windows.Forms.DataGridView();
+            this.linkUserActions = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.picture_smallPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEvent)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewActions)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picture_smallPictureBox
+            // 
+            this.picture_smallPictureBox.Location = new System.Drawing.Point(11, 63);
+            this.picture_smallPictureBox.Margin = new System.Windows.Forms.Padding(4);
+            this.picture_smallPictureBox.Name = "picture_smallPictureBox";
+            this.picture_smallPictureBox.Size = new System.Drawing.Size(267, 192);
+            this.picture_smallPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picture_smallPictureBox.TabIndex = 41;
+            this.picture_smallPictureBox.TabStop = false;
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Location = new System.Drawing.Point(11, 8);
+            this.buttonLogin.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(100, 28);
+            this.buttonLogin.TabIndex = 36;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // listBoxEvents
+            // 
+            this.listBoxEvents.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxEvents.DisplayMember = "name";
+            this.listBoxEvents.FormattingEnabled = true;
+            this.listBoxEvents.ItemHeight = 16;
+            this.listBoxEvents.Location = new System.Drawing.Point(286, 296);
+            this.listBoxEvents.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxEvents.Name = "listBoxEvents";
+            this.listBoxEvents.Size = new System.Drawing.Size(714, 180);
+            this.listBoxEvents.TabIndex = 40;
+            this.listBoxEvents.SelectedIndexChanged += new System.EventHandler(this.listBoxEvents_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(284, 13);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(84, 17);
+            this.label3.TabIndex = 44;
+            this.label3.Text = "Post Status:";
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxStatus.Location = new System.Drawing.Point(377, 10);
+            this.textBoxStatus.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.Size = new System.Drawing.Size(454, 22);
+            this.textBoxStatus.TabIndex = 45;
+            // 
+            // buttonSetStatus
+            // 
+            this.buttonSetStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSetStatus.Location = new System.Drawing.Point(841, 8);
+            this.buttonSetStatus.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSetStatus.Name = "buttonSetStatus";
+            this.buttonSetStatus.Size = new System.Drawing.Size(100, 28);
+            this.buttonSetStatus.TabIndex = 46;
+            this.buttonSetStatus.Text = "Post";
+            this.buttonSetStatus.UseVisualStyleBackColor = true;
+            this.buttonSetStatus.Click += new System.EventHandler(this.buttonSetStatus_Click);
+            // 
+            // listBoxPosts
+            // 
+            this.listBoxPosts.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxPosts.DisplayMember = "name";
+            this.listBoxPosts.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxPosts.FormattingEnabled = true;
+            this.listBoxPosts.ItemHeight = 24;
+            this.listBoxPosts.Location = new System.Drawing.Point(287, 63);
+            this.listBoxPosts.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxPosts.Name = "listBoxPosts";
+            this.listBoxPosts.Size = new System.Drawing.Size(713, 172);
+            this.listBoxPosts.TabIndex = 40;
+            this.listBoxPosts.SelectedIndexChanged += new System.EventHandler(this.listBoxPosts_SelectedIndexChanged);
+            // 
+            // pictureBoxEvent
+            // 
+            this.pictureBoxEvent.Location = new System.Drawing.Point(901, 528);
+            this.pictureBoxEvent.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBoxEvent.Name = "pictureBoxEvent";
+            this.pictureBoxEvent.Size = new System.Drawing.Size(99, 84);
+            this.pictureBoxEvent.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxEvent.TabIndex = 42;
+            this.pictureBoxEvent.TabStop = false;
+            // 
+            // labelEvents
+            // 
+            this.labelEvents.AutoSize = true;
+            this.labelEvents.LinkArea = new System.Windows.Forms.LinkArea(0, 13);
+            this.labelEvents.Location = new System.Drawing.Point(286, 257);
+            this.labelEvents.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.labelEvents.Name = "labelEvents";
+            this.labelEvents.Size = new System.Drawing.Size(225, 35);
+            this.labelEvents.TabIndex = 48;
+            this.labelEvents.TabStop = true;
+            this.labelEvents.Text = "Fetch Events \r\n(Click on an event to view it\'s picture)";
+            this.labelEvents.UseCompatibleTextRendering = true;
+            this.labelEvents.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.labelEvents_LinkClicked);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1217, 690);
+            this.tabControl1.TabIndex = 51;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.f_CheckinsButton);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.listBoxPostComments);
+            this.tabPage1.Controls.Add(this.pictureBoxEvent);
+            this.tabPage1.Controls.Add(this.buttonLogin);
+            this.tabPage1.Controls.Add(this.linkPosts);
+            this.tabPage1.Controls.Add(this.listBoxEvents);
+            this.tabPage1.Controls.Add(this.labelEvents);
+            this.tabPage1.Controls.Add(this.listBoxPosts);
+            this.tabPage1.Controls.Add(this.buttonSetStatus);
+            this.tabPage1.Controls.Add(this.picture_smallPictureBox);
+            this.tabPage1.Controls.Add(this.textBoxStatus);
+            this.tabPage1.Controls.Add(this.label3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage1.Size = new System.Drawing.Size(1209, 661);
+            this.tabPage1.TabIndex = 1;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            this.tabPage1.Click += new System.EventHandler(this.tabPage1_Click);
+            // 
+            // f_CheckinsButton
+            // 
+            this.f_CheckinsButton.Location = new System.Drawing.Point(1035, 104);
+            this.f_CheckinsButton.Name = "f_CheckinsButton";
+            this.f_CheckinsButton.Size = new System.Drawing.Size(125, 23);
+            this.f_CheckinsButton.TabIndex = 53;
+            this.f_CheckinsButton.Text = "Show Chekins";
+            this.f_CheckinsButton.UseVisualStyleBackColor = true;
+            this.f_CheckinsButton.Click += new System.EventHandler(this.f_CheckinsButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1035, 63);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 52;
+            this.button1.Text = "Show Friends";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.linkFriends_LinkClicked);
+            // 
+            // listBoxPostComments
+            // 
+            this.listBoxPostComments.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxPostComments.DisplayMember = "name";
+            this.listBoxPostComments.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBoxPostComments.FormattingEnabled = true;
+            this.listBoxPostComments.ItemHeight = 24;
+            this.listBoxPostComments.Location = new System.Drawing.Point(286, 484);
+            this.listBoxPostComments.Margin = new System.Windows.Forms.Padding(4);
+            this.listBoxPostComments.Name = "listBoxPostComments";
+            this.listBoxPostComments.Size = new System.Drawing.Size(570, 172);
+            this.listBoxPostComments.TabIndex = 51;
+            // 
+            // linkPosts
+            // 
+            this.linkPosts.AutoSize = true;
+            this.linkPosts.Location = new System.Drawing.Point(284, 43);
+            this.linkPosts.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkPosts.Name = "linkPosts";
+            this.linkPosts.Size = new System.Drawing.Size(82, 17);
+            this.linkPosts.TabIndex = 49;
+            this.linkPosts.TabStop = true;
+            this.linkPosts.Text = "Fetch Posts";
+            this.linkPosts.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkPosts_LinkClicked);
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.comboBoxActionType);
+            this.tabPage2.Controls.Add(this.dataGridViewActions);
+            this.tabPage2.Controls.Add(this.linkUserActions);
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(4);
+            this.tabPage2.Size = new System.Drawing.Size(1209, 661);
+            this.tabPage2.TabIndex = 2;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxActionType
+            // 
+            this.comboBoxActionType.FormattingEnabled = true;
+            this.comboBoxActionType.Items.AddRange(new object[] {
+            "music",
+            "television",
+            "movies",
+            "books",
+            "books.reads",
+            "books.wants_to_read",
+            "books.rates",
+            "books.quotes",
+            "fitness.runs",
+            "fitness.walks",
+            "fitness.bikes",
+            "music.listens",
+            "music.playlists",
+            "news.reads",
+            "news.publishes",
+            "video.watches",
+            "video.rates",
+            "videos.wants_to_watch"});
+            this.comboBoxActionType.Location = new System.Drawing.Point(160, 8);
+            this.comboBoxActionType.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxActionType.Name = "comboBoxActionType";
+            this.comboBoxActionType.Size = new System.Drawing.Size(219, 24);
+            this.comboBoxActionType.TabIndex = 23;
+            // 
+            // dataGridViewActions
+            // 
+            this.dataGridViewActions.AllowUserToAddRows = false;
+            this.dataGridViewActions.AllowUserToDeleteRows = false;
+            this.dataGridViewActions.AllowUserToOrderColumns = true;
+            this.dataGridViewActions.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewActions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewActions.Location = new System.Drawing.Point(15, 40);
+            this.dataGridViewActions.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridViewActions.Name = "dataGridViewActions";
+            this.dataGridViewActions.ReadOnly = true;
+            this.dataGridViewActions.RowHeadersWidth = 51;
+            this.dataGridViewActions.Size = new System.Drawing.Size(1087, 703);
+            this.dataGridViewActions.TabIndex = 22;
+            // 
+            // linkUserActions
+            // 
+            this.linkUserActions.AutoSize = true;
+            this.linkUserActions.Location = new System.Drawing.Point(11, 11);
+            this.linkUserActions.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.linkUserActions.Name = "linkUserActions";
+            this.linkUserActions.Size = new System.Drawing.Size(127, 17);
+            this.linkUserActions.TabIndex = 14;
+            this.linkUserActions.TabStop = true;
+            this.linkUserActions.Text = "Fetch User Actions";
+            this.linkUserActions.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkUserActions_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1217, 690);
+            this.Controls.Add(this.tabControl1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picture_smallPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxEvent)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewActions)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox picture_smallPictureBox;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.ListBox listBoxEvents;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Button buttonSetStatus;
+        private System.Windows.Forms.ListBox listBoxPosts;
+        private System.Windows.Forms.PictureBox pictureBoxEvent;
+        private System.Windows.Forms.LinkLabel labelEvents;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.ComboBox comboBoxActionType;
+        private System.Windows.Forms.DataGridView dataGridViewActions;
+        private System.Windows.Forms.LinkLabel linkUserActions;
+        private System.Windows.Forms.ListBox listBoxPostComments;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.LinkLabel linkPosts;
+        private System.Windows.Forms.Button f_CheckinsButton;
+    }
+}
+
